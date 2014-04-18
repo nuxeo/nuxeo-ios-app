7af301af555e6384305368e91d5bebcdc46b7254
Matthias ROUBEROL <matthias@smartnsoft.com>
2014-04-18 10:37:37 +0200

    Updates constants with local host url for nuxeo server in debug mode.

--- nuxeo-ios-app/Classes/Constants.h
+++ nuxeo-ios-app/Classes/Constants.h
@@ -148,7 +148,7 @@
 
 /********************************** NUXEO SETTINGS ********************************/
 #ifdef DEBUG
-#define kNuxeoSiteURL               @"http://smartnsoft-nuxeo.sales.nuxeo.io/nuxeo/"
+#define kNuxeoSiteURL               @"http://localhost:8080/nuxeo/"
 #else
 #define kNuxeoSiteURL               @"http://demo.nuxeo.com/nuxeo/"
 #endif
